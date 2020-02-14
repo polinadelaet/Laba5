40e8b4de7459e5b5e8b8fe8395b424711b3e18f7
Lina <polina.usui@gmail.com>
2020-02-14 22:18:48 +0300

    Initial commit

---
+++ src/main/java/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("pizdec");
+        int an = 64;
+        System.out.println(an);
+    }
+}
