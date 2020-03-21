3598ce6fb2a3e45273c51ef4460fc56e7cb087b2
Lina <polina.usui@gmail.com>
2020-03-21 20:10:27 +0300

    Доделала ScriptReader

--- src/main/java/app/commands/script/ExecuteLine.java
+++ src/main/java/app/commands/script/ExecuteLine.java
@@ -5,12 +5,6 @@ import java.util.List;
 
 public class ExecuteLine {
 
-    private List<String> lines;
-    private String filePath;
-
-    public ExecuteLine(List<String> lines) {
-        this.lines = lines;
-    }
 
     public void execute(Script script) throws FileCreationException {
         while (script.hasNextLine()){
