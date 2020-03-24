c53beb56d78064866f29e97ac87d6ee039574fe2
Alexey <leshaego14@gmail.com>
2020-03-24 16:50:36 +0300

    New TODOs

--- src/main/java/app/controller/Controller.java
+++ src/main/java/app/controller/Controller.java
@@ -4,6 +4,11 @@ import app.query.Query;
 import app.response.Response;
 
 public final class Controller {
-    //public Response handleQuery(Query query){
-    //}
+
+    //TODO: 1) Создать по запросу команду через фабрику. Раз ты не валидируешь запросы к серверу, то проверок здесь не нужно.
+    //TODO: 2) Выполнить команду и получить ее ответ.
+    //TODO: 3) Вернуть ответ из метода
+    public Response handleQuery(Query query){
+        return null;
+    }
 }
