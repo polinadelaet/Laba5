67930772df32ff62acc124fb74b9dd15e1a893c9
Alexey <leshaego14@gmail.com>
2020-03-29 23:43:45 +0300

    Small additing to deploy.

--- deploy/setupAndRun.bat
+++ deploy/setupAndRun.bat
@@ -5,4 +5,5 @@ cd build\distributions\
 "C:\Program Files\7-Zip\7zG" x Laba5-1.0-SNAPSHOT.zip -y
 copy "bin\*" "Laba5-1.0-SNAPSHOT\bin\"
 del Laba5-1.0-SNAPSHOT\bin\Laba5 Laba5-1.0-SNAPSHOT\bin\Laba5.bat
+SET WC_FILE="./files/workerCollection"
 call Laba5-1.0-SNAPSHOT\bin\Laba5.bat
